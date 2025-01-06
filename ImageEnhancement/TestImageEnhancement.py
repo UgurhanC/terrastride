9ccd12a9f4c4128f6f089703c3756689e7e3290f
SussyMongoosey <150016091+SussyMongoosey@users.noreply.github.com>
2025-01-06 14:42:01 +0100

    Changes with testing
    Changed some executable code to achieve testing on different subjects

--- ImageEnhancement/TestImageEnhancement.py
+++ ImageEnhancement/TestImageEnhancement.py
@@ -9,25 +9,31 @@ with open('best_params.json') as f:
 
 url_header = os.path.dirname(__file__)
 test_image_url = url_header + r"\Data\LowLight\18.png"
-test_image_url2 = url_header + r"\Data\LowLight\1.png"
+#test_image_url2 = url_header + r"\Data\LowLight\1.png"
 
 test_image1 = cv2.imread(test_image_url)
 enhanced_image1 = apply_image_enhancement(test_image1,best_params)
 
-test_image2 = cv2.imread(test_image_url2)
-enhanced_image2 = apply_image_enhancement(test_image2,best_params)
+#test_image2 = cv2.imread(test_image_url2)
+#enhanced_image2 = apply_image_enhancement(test_image2,best_params)
 
-fig, ax = plt.subplots(2,2)
+#fig, ax = plt.subplots(1,2)
 
-ax[0,0].imshow(cv2.cvtColor(test_image1,cv2.COLOR_BGR2RGB))
+"""ax[0,0].imshow(cv2.cvtColor(test_image1,cv2.COLOR_BGR2RGB))
 ax[0,0].axis("off")
 ax[0,1].imshow(cv2.cvtColor(enhanced_image1,cv2.COLOR_BGR2RGB))
-ax[0,1].axis("off")
+ax[0,1].axis("off")"""
 
-ax[1,0].imshow(cv2.cvtColor(test_image2,cv2.COLOR_BGR2RGB))
+"""ax[0].imshow(cv2.cvtColor(test_image1,cv2.COLOR_BGR2RGB))
+ax[0].axis("off")"""
+plt.imshow(cv2.cvtColor(enhanced_image1,cv2.COLOR_BGR2RGB))
+plt.axis("off")
+
+"""ax[1,0].imshow(cv2.cvtColor(test_image2,cv2.COLOR_BGR2RGB))
 ax[1,0].axis("off")
 ax[1,1].imshow(cv2.cvtColor(enhanced_image2,cv2.COLOR_BGR2RGB))
 ax[1,1].axis("off")
 
-fig.tight_layout()
+"""
+plt.tight_layout()
 plt.show()
