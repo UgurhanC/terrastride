cb2754e11ccb48f2d59eddd230ea35e1342eec91
damidirad <85406408+damidirad@users.noreply.github.com>
2025-01-14 15:48:43 +0100

    Update detection.py

--- basic_pipelines/detection.py
+++ basic_pipelines/detection.py
@@ -64,7 +64,6 @@ def app_callback(pad, info, user_data):
         if label == "person":
             string_to_print += f"Detection: {label} {confidence:.2f}\n"
             detection_count += 1
-    print(det)
     if user_data.use_frame:
         # Note: using imshow will not work here, as the callback function is not running in the main thread
         # Let's print the detection count to the frame
